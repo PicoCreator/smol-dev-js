ea85af6d2942b03d12bc8de65508d04887adbf77
Eugene Cheah <eugeneqin@gmail.com>
2023-05-17 15:44:43 +0000

    readFileOrNull

---
+++ src/util/readFileOrNull.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+/**
+ * Read and return the file content if the file exists, otherwise return null
+ * @param {String} filepath 
+ * @param {String} fallback
+ */
+module.exports = async function readFileOrNull(filepath, fallback = null) {
+    try {
+        return fs.promises.readFile(filepath, "utf8");
+    } catch(e) {
+        // do nothing
+    }
+    return fallback;
+}
